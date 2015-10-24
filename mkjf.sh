5f187161519312411f534d4504def76bbf96dc1c
Yosuke TANIGAWA <yk.tanigawa@gmail.com>
2015-10-24 20:46:18 +0900

    update mkjf.sh to use xargs for parallel computation

--- mkjf.sh
+++ mkjf.sh
@@ -2,20 +2,14 @@
 
 # compute k-mer database for givin k using jellyfish
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <target dir> <k>" 1>&2
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <target dir> <k> <parallel>" 1>&2
     exit 1
 fi
 
 dir=${1}
 k=${2}
+P=${3}
+jellyfish="${HOME}/bin/jellyfish"
 
-for file in `find ${dir} -name *.fasta`; do
-    if [ ! -e ${file%.fasta}.${k}.jf ]; then
-	jellyfish count -m ${k} -s 100M -t 6 ${file} -o ${file%.fasta}.${k}.jf &
-	#echo "jellyfish count -m ${k} -s 100M -t 6 ${file} -o ${file%.fasta}.${k}.jf"
-    fi
-done
-
-wait
-
+find ${dir} -type f -name '*.fasta' | xargs -n1 -P${P} -I@ ${jellyfish} count -m ${k} -s 100M -t 6 @ -o @.${k}.jf
